fc93c5afd4adf25628bb75416e4eac1eec9129f4
programlanguage <programlanguages@gmail.com>
2023-10-16 17:08:48 +0400

    doctorPersonalPage is made

--- src/navigation/AuthNavigate.tsx
+++ src/navigation/AuthNavigate.tsx
@@ -8,6 +8,8 @@ import Notification from '../screens/notification/notification';
 import Doctorlist from '../screens/doctorList/doctorlist';
 import UserHomePage from '../screens/userHomePage/userhomepage';
 import OTPScreen from '../screens/register/OTPScreen';
+import DoctorPersonalPage from '../screens/doctorPersonalPage.tsx/doctorPersonalPage';
+
 type RootStackParamList = {
   LoginScreen: undefined;
   SignUpScreen: undefined;
@@ -17,6 +19,7 @@ type RootStackParamList = {
   Doctorlist: undefined;
   UserHomePage: undefined;
   OTPScreen: undefined;
+  DoctorPersonalPage: undefined;
 };
 
 const AuthNavigator = createStackNavigator<RootStackParamList>();
@@ -30,15 +33,17 @@ export function AuthNavigate() {
     <AuthNavigator.Navigator screenOptions={screenOptions}>
       {/* <AuthNavigator.Screen name="LoginScreen" component={LoginScreen} /> */}
       {/* <AuthNavigator.Screen name="SignUpScreen" component={SignUpScreen} /> */}
-      <AuthNavigator.Screen name="OTPScreen" component={OTPScreen} />
+      <AuthNavigator.Screen name="Doctorlist" component={Doctorlist} />
+
+      <AuthNavigator.Screen name="DoctorPersonalPage" component={DoctorPersonalPage} />
+      {/* <AuthNavigator.Screen name="OTPScreen" component={OTPScreen} /> */}
       {/* <AuthNavigator.Screen name="AboutPage" component={About} /> */}
       {/* <AuthNavigator.Screen name="AddToCard" component={AddToCard} /> */}
       {/* <AuthNavigator.Screen name="Notification" component={Notification} /> */}
-      <AuthNavigator.Screen name="UserHomePage" component={UserHomePage} />
+      {/* <AuthNavigator.Screen name="UserHomePage" component={UserHomePage} /> */}
       {/* <AuthNavigator.Screen name="AboutPage" component={About} /> */}
       {/* <AuthNavigator.Screen name="AddToCard" component={AddToCard} /> */}
       {/* <AuthNavigator.Screen name="Notification" component={Notification} /> */}
-      {/* <AuthNavigator.Screen name="Doctorlist" component={Doctorlist} /> */}
     </AuthNavigator.Navigator>
   );
 }
