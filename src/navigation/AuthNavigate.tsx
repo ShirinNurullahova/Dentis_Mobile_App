08f58c270968e3a7f1dda3807cb270354f5cf030
programlanguage <programlanguages@gmail.com>
2023-10-17 10:35:10 +0400

    aboutDetail page is made

--- src/navigation/AuthNavigate.tsx
+++ src/navigation/AuthNavigate.tsx
@@ -6,11 +6,12 @@ import About from '../screens/about/about';
 import AddToCard from '../screens/addToCard/addToCard';
 import Notification from '../screens/notification/notification';
 import Doctorlist from '../screens/doctorList/doctorlist';
-import OTPScreen from '../screens/register/OTPScreen'
+import OTPScreen from '../screens/register/OTPScreen';
 import ResetPasswordScreen from '../screens/register/ResetPasswordScreen';
 import UserHomePage from '../screens/userHomePage/userhomepage';
 import HomePage from '../screens/homePage/homePage';
 import DoctorPersonalPage from '../screens/doctorPersonalPage.tsx/doctorPersonalPage';
+import DoctorDetail from '../screens/doctorDetail/doctordetail';
 
 type RootStackParamList = {
   LoginScreen: undefined;
@@ -19,11 +20,12 @@ type RootStackParamList = {
   AddToCard: undefined;
   Notification: undefined;
   Doctorlist: undefined;
-  ResetPasswordScreen: undefined
+  ResetPasswordScreen: undefined;
   UserHomePage: undefined;
-  HomePage: undefined
+  HomePage: undefined;
   OTPScreen: undefined;
   DoctorPersonalPage: undefined;
+  DoctorDetail: undefined;
 };
 
 const AuthNavigator = createStackNavigator<RootStackParamList>();
@@ -37,15 +39,16 @@ export function AuthNavigate() {
     <AuthNavigator.Navigator screenOptions={screenOptions}>
       {/* <AuthNavigator.Screen name="LoginScreen" component={LoginScreen} /> */}
       {/* <AuthNavigator.Screen name="SignUpScreen" component={SignUpScreen} /> */}
-      <AuthNavigator.Screen name="Doctorlist" component={Doctorlist} />
-      <AuthNavigator.Screen name="DoctorPersonalPage" component={DoctorPersonalPage} />
+      {/* <AuthNavigator.Screen name="Doctorlist" component={Doctorlist} /> */}
+      <AuthNavigator.Screen name="DoctorDetail" component={DoctorDetail} />
+      {/* <AuthNavigator.Screen name="DoctorPersonalPage" component={DoctorPersonalPage} /> */}
       {/* <AuthNavigator.Screen name="OTPScreen" component={OTPScreen} /> */}
       {/* <AuthNavigator.Screen name="ResetPasswordScreen" component={ResetPasswordScreen} /> */}
       {/* <AuthNavigator.Screen name="AboutPage" component={About} /> */}
       {/* <AuthNavigator.Screen name="AddToCard" component={AddToCard} /> */}
       {/* <AuthNavigator.Screen name="Notification" component={Notification} /> */}
       {/* <AuthNavigator.Screen name="UserHomePage" component={UserHomePage} /> */}
-       {/* <AuthNavigator.Screen name="HomePage" component={HomePage} /> */}
+      {/* <AuthNavigator.Screen name="HomePage" component={HomePage} /> */}
       {/* <AuthNavigator.Screen name="AboutPage" component={About} /> */}
       {/* <AuthNavigator.Screen name="AddToCard" component={AddToCard} /> */}
       {/* <AuthNavigator.Screen name="Notification" component={Notification} /> */}
